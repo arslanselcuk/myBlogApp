054896ad646727bab626de461366dc23ccb094a6
selcuk a <wwwajan@yahoo.com>
2019-12-16 16:31:39 +0100

    last status

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
         
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+        
 class Deneme(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -30,3 +33,19 @@ class Deneme(models.Model):
 
     def __str__(self):
         return self.title        
+        
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')    # The related_name option in models.ForeignKey allows us to have access to comments from within the Post model.    
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text        
+        
+    
